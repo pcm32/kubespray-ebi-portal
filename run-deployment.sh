f29caec146d254df88b7c1b245ec818d394e8430
Pablo Moreno <pablo.a.moreno@gmail.com>
2018-02-18 11:52:55 +0000

    Adds config sample and moves image selection to run-deployments, as this has been tested only with that image choice.

--- run-deployment.sh
+++ run-deployment.sh
@@ -2,6 +2,19 @@
 source $PWD/openstack.rc
 source $PWD/config.sh
 
+## OS images for k8s and glusterfs
+# This image is currently uploaded if not in the project.
+## Base image to use, currently the same for Kubernetes machines and GFS. Probably should move inside.
+export BASE_IMAGE_VERSION="current"
+export BASE_IMAGE_URL="https://cloud-images.ubuntu.com/xenial/$BASE_IMAGE_VERSION/xenial-server-cloudimg-amd64-disk1.img"
+export BASE_IMAGE_NAME_PREFIX="Ubuntu-16.04"
+
+export TF_VAR_image=$BASE_IMAGE_NAME_PREFIX-$BASE_IMAGE_VERSION
+export TF_VAR_ssh_user="ubuntu" # needs to be adequate to the BASE_IMAGE_SET
+
+export TF_VAR_image_gfs=$BASE_IMAGE_NAME_PREFIX-$BASE_IMAGE_VERSION
+export TF_VAR_ssh_user_gfs="ubuntu"
+
 # Paths to public and private keys for the deployment
 export PUBLIC_KEY="$PWD/keys/key.pub"
 export PRIVATE_KEY="$PWD/keys/key.pem"
