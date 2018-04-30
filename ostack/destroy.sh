df9e016e058d5a564960501fe0fef58f3cc486cd
Pablo Moreno <pablo.a.moreno@gmail.com>
2018-04-30 19:43:43 +0100

    Adds terraform init for destroy.

--- ostack/destroy.sh
+++ ostack/destroy.sh
@@ -4,8 +4,13 @@ set -e
 # The script assumes that env vars for authentication with OpenStack are already present.
 
 ## Customise this ##
+export PORTAL_APP_REPO_FOLDER="$PWD"
+export KARGO_TERRAFORM_FOLDER=$PORTAL_APP_REPO_FOLDER'/kubespray/contrib/terraform/openstack'
 export TF_VAR_DEPLOYMENT_KEY_PATH=$PORTAL_DEPLOYMENTS_ROOT'/'$PORTAL_DEPLOYMENT_REFERENCE'/'$PORTAL_DEPLOYMENT_REFERENCE'.pub'
 
 # Destroy everything
 #cd ostack/terraform || exit
+cd $PORTAL_APP_REPO_FOLDER'/kubespray'
+#terraform plan --state=$PORTAL_DEPLOYMENTS_ROOT'/'$PORTAL_DEPLOYMENT_REFERENCE'/terraform.tfstate' $KARGO_TERRAFORM_FOLDER
+terraform init $KARGO_TERRAFORM_FOLDER
 terraform destroy --force --input=false --state=$PORTAL_DEPLOYMENTS_ROOT'/'$PORTAL_DEPLOYMENT_REFERENCE'/terraform.tfstate'
--- run-destroy.sh
+++ run-destroy.sh
@@ -3,6 +3,7 @@ source $PWD/openstack.rc
 source $PWD/config.sh
 
 # Vars for the operation of the deployment
+export PORTAL_APP_REPO_FOLDER="$PWD"
 export PORTAL_DEPLOYMENTS_ROOT=$PWD/deployments
 
 bash ostack/destroy.sh
